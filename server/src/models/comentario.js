401585eb3cba6a9720f0d1a35b42b4c1bab1aa4f
David López <133236822+Davidleo03@users.noreply.github.com>
2025-04-12 21:12:12 +0000

    v9

--- server/src/models/comentario.js
+++ server/src/models/comentario.js
@@ -3,9 +3,9 @@ import pool from '../db/database.js';
 export const createComentario = async (nombre, comentario, fecha) => {
   try {
     const result = await pool.query(`INSERT INTO comentarios (nombre, comentario, fecha)
-VALUES ($1, $2, $3);`),
-      [nombre, comentario, fecha]);
-    console.log(result.rows[0])
+VALUES ($1, $2, $3);`, [nombre, comentario, fecha])
+      
+    
     return result.rows[0]
 
   } catch (e) {
